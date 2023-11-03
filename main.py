d2b1f7a5d3dd258d200603d7586be64fdb0d7ceb
J. F. Dooley <jfdoolster@gmail.com>
2023-11-02 22:53:00 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+def moving_average(arr: np.ndarray, n=3):
+    ret = np.cumsum(arr, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    nan_arr = np.empty((n-1,))
+    nan_arr[:] = np.nan
+    return np.hstack((nan_arr, np.array(ret[n-1:]/n)))
+
+def gradient_filter(arr: np.ndarray, std_cutoff=0.2):
+    ret = np.gradient(arr)
+    avg, std = np.nanmean(ret), np.nanstd(ret)
+    cutoff = std_cutoff * std
+    mask = (ret > (avg-cutoff)) & (ret < (avg+cutoff))
+    return mask
+
+
+if __name__=="__main__":
+
+    xdata=np.linspace(0,100,75)
+
+    smoothed=moving_average(xdata)
+    grad_mask = gradient_filter(smoothed)
+    # todo split
+
+    print(len(smoothed))
+    print(len(grad_mask))
+
