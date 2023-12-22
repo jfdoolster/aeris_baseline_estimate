5a98404ec36eaba94c0ecd2cbf513e6354eb31ba
J. F. Dooley <jfdoolster@gmail.com>
2023-12-21 22:04:25 -0700

    update with sigma

--- baseline_plotter.py
+++ baseline_plotter.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import *
 
 def baseline_correction_plotter(df: pd.DataFrame, d: dict) -> plt.Figure:
     plt.rcParams['axes.grid'] = True
@@ -40,10 +41,19 @@ def baseline_correction_plotter(df: pd.DataFrame, d: dict) -> plt.Figure:
     # plot gradient of rawdata and unfiltered samples
     ax1 = fig.add_subplot(222, sharex=ax0)
     ax1.plot(df['seconds'], df['segment_gradient'], color=gradient_color, label=fr"$\nabla {CVAR}$")
-    vertical_max = (d['gradient_filter_std_threshold'] + 0.5 ) * np.std(df['segment_gradient'])
+    #vertical_max = (d['gradient_filter_std_threshold'] * np.std(df['segment_gradient'])) * 2.00
+    vertical_max = max(abs(df[df['gradient_mask']]['segment_gradient']).dropna())
     ax1.plot(df[df['gradient_mask']]['seconds'], df[df['gradient_mask']]['segment_gradient'], \
         ls='None', marker='o', color=fitted_color, label=gradfilt_str)
-    ax1.set_ylim(-vertical_max, vertical_max)
+
+    ax1.set_ylim(-vertical_max*2.00, vertical_max*2.00)
+
+    def y_fmt(x, y):
+        #return f"${x/np.std(df['segment_gradient']):.1f}\,$"+"$\sigma$"
+        return f"${(d['gradient_filter_std_threshold']*x/vertical_max):.1f}\,$"+"$\sigma$"
+
+    ax1.yaxis.set_major_locator(MultipleLocator(vertical_max))
+    ax1.yaxis.set_major_formatter(FuncFormatter(y_fmt))
 
     # plot gradient of rawdata and unfiltered samples
     ax2 = fig.add_subplot(223, sharex=ax0, sharey=ax0)
