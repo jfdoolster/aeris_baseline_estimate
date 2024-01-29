36104d1e36fe66436cbf6016e345f8da46a6c56f
archie <jfdoolster@gmail.com>
2024-01-29 13:37:24 -0700

    -marchie commit 1/29

--- polynomial.py
+++ polynomial.py
@@ -21,7 +21,7 @@ def polynomial_regression(x_arr: np.ndarray, y_arr: np.ndarray, power=1, force_z
 
     if xx_determinant == 0.0:
         print("Singluar Matrix!")
-        return np.empty((power+1,))
+        return np.empty((power+1,1)), np.empty((len(y_arr),1))
 
     xx_inv = np.linalg.inv(x_cross_xprime)
     xx_cross_xp = np.matmul(xx_inv, x_mat_prime)
